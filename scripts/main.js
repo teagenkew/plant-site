01468ab31924375a50dd8756dc7c2b67e0635bcc
teagenkew <96027598+teagenkew@users.noreply.github.com>
2024-10-17 14:24:55 -0600

    adds array to get all plantImageUrls to be used later as plant placeholders

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,18 @@ const backend = new PlantsAPI("plants", 9);
 async function fetchPlants() {
   try {
     const response = await backend.getPlants();
+    return response;
   } catch (error) {
     console.log(error);
   }
 }
-fetchPlants();
+
+const plants = await fetchPlants();
+console.log("plants", plants);
 
 const plantImageUrls = [];
+plants.forEach((plant) => {
+  plantImageUrls.push(plant.url);
+});
+
+console.log(plantImageUrls);
