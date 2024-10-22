f8a26ce4f1c6878d28ea186ed14f759ba92b0742
teagenkew <96027598+teagenkew@users.noreply.github.com>
2024-10-22 17:00:19 -0600

    adds plant images using pexels api

--- scripts/main.js
+++ scripts/main.js
@@ -14,8 +14,33 @@ const plants = await fetchPlants();
 console.log("plants", plants);
 
 const plantImageUrls = [];
+const plantNames = [];
 plants.forEach((plant) => {
-  plantImageUrls.push(plant.url);
+  plantImageUrls.push(plant.src.tiny);
+  plantNames.push(plant.photographer);
 });
 
 console.log(plantImageUrls);
+console.log(plantNames);
+
+async function addPlantCards() {
+  const newPlants = document.querySelector(".new-plants__cards");
+  newPlants.innerHTML = "";
+  for (let i = 0; i < plantImageUrls.length; i++) {
+    const plantCard = document.createElement("article");
+    plantCard.classList.add("plant-card");
+
+    const plantImage = document.createElement("img");
+    plantImage.classList.add("plant-card__image");
+    const plantTitle = document.createElement("h3");
+    plantTitle.classList.add("plant-card__title");
+    plantTitle.innerText = plantNames[i];
+    plantImage.src = plantImageUrls[i];
+
+    plantCard.appendChild(plantImage);
+    plantCard.appendChild(plantTitle);
+    newPlants.appendChild(plantCard);
+  }
+}
+
+addPlantCards();
